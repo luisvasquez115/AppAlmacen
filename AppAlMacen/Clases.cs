b8d8ab8bb9d3ff1ca783d076ced7bcf222dd3206
luis angel <luisdelarosa115@gmail.com>
2020-01-04 12:38:49 -0400

    Proyecto creado sin modificaciones graves, antes de poner fragment

---
+++ AppAlMacen/Clases.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V4.App;
+using Android.Views;
+using Android.Widget;
+
+namespace AppAlmacen
+{
+    public class MyNavigationItemSelectedListener : Java.Lang.Object, NavigationView.IOnNavigationItemSelectedListener
+    {
+        Context context;
+        public MyNavigationItemSelectedListener(Context context)
+        {
+            this.context = context;
+        }
+
+
+        bool NavigationView.IOnNavigationItemSelectedListener.OnNavigationItemSelected(IMenuItem menuItem) 
+        {
+            int id = menuItem.ItemId;
+
+            if (id == Resource.Id.nav_camera)
+            {
+                Intent intent = new Intent(context, typeof(Actividades.Activity1));  //the activity you want to open
+                context.StartActivity(intent);
+
+            }
+
+
+            return true;
+        }
+    }
+}
